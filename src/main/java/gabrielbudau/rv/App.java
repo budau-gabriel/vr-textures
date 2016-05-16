15a9b52b7bb4bcdc2613a2bfcfe76fa3689596b0
Gabriel Budau <gabrielbudau@protonmail.ch>
2016-05-16 21:58:16 +0300

    initial commit

---
+++ src/main/java/gabrielbudau/rv/App.java
@@ -0,0 +1,13 @@
+package gabrielbudau.rv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
